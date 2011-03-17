a144a93565a18c2fb5465ec211e1ec204ae7ea39
Jonathan Birge <birge@mit.edu>
2011-03-17 02:35:00 -0400

    help comments for GUI functions

--- getdata.m
+++ getdata.m
@@ -1,3 +1,6 @@
 function data = getdata(tag, prop)
+%GETDATA Retrieve property from GUI given a tag name.
+%  This function is mean to be called from a callback, as it uses gcbf.
+
 handle = findobj(gcbf, 'Tag', tag);
 data = get(handle, prop);
--- gethandle.m
+++ gethandle.m
@@ -1,5 +1,9 @@
 function hdl = gethandle(tag)
+%GETHANDLE  Retrieve handles of objects given a tag name.
+%  Will retrieve a list of handles given a cell array of tags.
+
 if iscell(tag)
+   hdl = zeros(1, length(tag));
    parfor i = 1:length(tag),
       hdl(i) = findobj(gcbf, 'Tag', tag{i});
    end
