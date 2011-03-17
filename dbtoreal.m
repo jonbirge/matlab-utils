47a9902323c3087e7950ec4d31a04e9f0f5b42ee
Jonathan Birge <birge@mit.edu>
2011-03-17 00:53:30 -0400

    updated docs for dbtoreal.m et al

--- dbtoreal.m
+++ dbtoreal.m
@@ -1,3 +1,4 @@
 function y = dbtoreal(x)
+%DBTOREAL Comvert from decibel to linear scale.
 
 y = 10.^(x/10);
--- realtodb.m
+++ realtodb.m
@@ -1,2 +1,4 @@
 function y = realtodb(x)
+%REALTODB Convert from linear to decibel scale.
+
 y = 10*log10(x);
