36fd4f878b3f87f2ef0da629addfd68c3ab9718d
Jonathan Birge <birge@mit.edu>
2011-03-17 02:50:19 -0400

    more help comments

--- pbino.m
+++ pbino.m
@@ -1,5 +1,5 @@
 function p = pbino(r, n, pi)
-%PBINO  Exact binomial distribution.
+%PBINO Exact binomial distribution.
 %  Compute binomial PDF in parallel, and in a simpler way that in the stats
 %  toolbox. This does not have all the input checking of the stats toolbox
 %  version, however, nor is it valid for very large n.
