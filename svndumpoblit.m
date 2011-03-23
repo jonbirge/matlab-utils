c3e83783fa1576ea920429862d7f5859a6820d9d
Jonathan Birge <birge@mit.edu>
2011-03-23 00:10:55 -0400

    fixed binary bug with new copy/scan code

--- svndumpoblit.m
+++ svndumpoblit.m
@@ -1,7 +1,7 @@
 function svndumpoblit(exsuff, filein, fileout)
 
 if nargin < 1 || isempty(exsuff)
-  exsuff = {'\.mexw32', '\.mexglx', '\.mexmac', '\.mexmaci64', '\.mexa64', '\.pdf'};
+  exsuff = {'\.mexw32', '\.mexglx', '\.mexmac', '\.mexmaci64', '\.mexa64', 'mex\.dll'};
 end
 
 if nargin < 2
@@ -17,35 +17,65 @@ end
 if iscell(exsuff)
   testregexp = [exsuff{1} '$'];
   for k = 2:length(exsuff)
-    testregexp = [testregexp '|' exsuff{k} '$'];
+    testregexp = [testregexp '|' exsuff{k}];
   end
 else
-  testregexp = [exsuff '$'];
+  testregexp = exsuff;
 end
+
+revtoken = 'Revision-number:';
+nodetoken = 'Node-path:';
+sizetoken = 'Text-content-length:';
+proptoken = 'PROPS-END';
+revtoklen = length(revtoken);
+nodetoklen = length(nodetoken);
+sizetoklen = length(sizetoken);
+proptoklen = length(proptoken);
 
 fin = fopen([pathin fnamein]);
 fout = fopen([pathout fnameout], 'w');
 
-copyflag = true;  % copy while true
+copyflag = true;  %#ok<*NASGU> % copy while true
+contlen = 0;
 if fin ~= -1
   dline = fgets(fin);
   while ischar(dline)
-    if strncmp(dline, 'Revision-number:', 16)
+    if strncmp(dline, revtoken, revtoklen)
       copyflag = true;
-      rev = str2double(dline(17:end));
+      rev = str2double(dline(revtoklen+1:end));
       fprintf('Revision: %d\n', rev);
-    elseif strncmp(dline, 'Node-path:', 10)  % quick test for add
+    elseif strncmp(dline, nodetoken, nodetoklen)  % test for node
       if ~isempty(regexp(dline, testregexp, 'once'))  % test for file
         copyflag = false;
       else
         copyflag = true;
       end
+    elseif strncmp(dline, sizetoken, sizetoklen)
+      contlen = str2double(dline(sizetoklen+1:end));
     end
+    
     if copyflag
       fprintf(fout, '%s', dline);
     end
+
+    if contlen > 0 && strncmp(dline, proptoken, proptoklen)  % handle content
+      if copyflag
+        copynbytes(fin, fout, contlen)
+      else
+        fprintf('skipping %g kB...\n', contlen/1000);
+        fseek(fin, contlen, 'cof');
+      end
+      contlen = 0;
+    end
+    
     dline = fgets(fin);
   end
+  
   fclose(fin);
   fclose(fout);
 end
+
+
+function copynbytes(fin, fout, n)
+block = fread(fin, n, 'char');
+fwrite(fout, block, 'char');
