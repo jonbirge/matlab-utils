c69832d32d3f794ab02e4f0f24c3310b12e9a9cd
Jonathan Birge <birge@mit.edu>
2011-09-12 23:08:58 -0400

    added gitcom.m

---
+++ gitcom.m
@@ -0,0 +1,24 @@
+function gitcom(logstr)
+%GITCOM Commit all changes in current MATLAB directory to git
+%  This prompts the user for a log string (also giving the option to abort)
+%  and then commits all changes in the working directory to git.
+
+if nargin == 0
+  logstr = input('enter log message: ', 's');
+end
+
+if length(logstr) > 1
+  gitcmd = ['git commit -a -m "' logstr '"'];
+  fprintf('running command: %s\n', gitcmd);
+  [status, result] = system(gitcmd);
+  if status == 0
+    fprintf('git commit:\n')
+    fprintf(result)
+  else
+    error('gitcom:git', result)
+  end
+else
+  fprintf('git commit aborted!\n');
+end
+
+end
