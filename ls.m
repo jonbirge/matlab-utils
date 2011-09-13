71c7ce7cd85644eccffaee58242b503496a59f42
Jonathan Birge <birge@mit.edu>
2011-09-13 12:22:45 -0400

    beginning changes to presort
    terminal output works, unsorted
    
    sorting works
    
    ls.m outputs to variable

--- ls.m
+++ ls.m
@@ -1,4 +1,4 @@
-function ls(files, showi)
+function filelist = ls(files, showi)
 %LS list directory with formatting.
 %  ls returns a directory listing in the current directory, or in an
 %  optional path given. The output is sorted into columns, ASV files are
@@ -11,81 +11,91 @@ function ls(files, showi)
 %  See also CD, DIR, WHAT, TYPE.
 
 
-% Hard coded settings.
+%%% Hard coded settings.
 ncols = 3;
-maxlen = 28;
-colwidth = 90;
+maxlen = 32;
+colwidth = ncols*maxlen;
 marginstr = '  ';
-fillstr = ' ';  % MUST be only one character
+fillstr = '.';  % MUST be only one character or an escape sequence
 contstr = ':';
 hidedots = true;
 hideasv = true;
 summary = false;
 countdir = true;  % count directory size
 depthlimit = 1;  % how far to recurse directories
+showvc = false;
 
-% Input handling.
+%%% Input handling.
 if nargin == 0
   files = '.';
 end
 if nargin < 2
   showi = false;
 end
+if nargout > 0
+  printq = false;
+else
+  printq = true;
+end
 
-% Variable init.
+%%% Variable init.
 sizesum = 0;
 filecount = 0;
 dircount = 0;
 contlen = length(contstr);
 
-% Display directory first.
+%%% Display directory first.
 cdir = pwd;
 if length(cdir) > colwidth;
-    slashes = find((cdir == '\') | (cdir == '/'));
-    wd = cdir(slashes(end-1):end);
-    head = cdir(1:slashes(3));
-    cdir = [head '...' wd];
+  slashes = find((cdir == '\') | (cdir == '/'));
+  wd = cdir(slashes(end-1):end);
+  head = cdir(1:slashes(3));
+  cdir = [head '...' wd];
 end
-if ~strcmp(files, '.')
-  fprintf('\n%s\n', [cdir '/' files])
-else
-  fprintf('\n%s\n', cdir)
+if printq
+  if ~strcmp(files, '.')
+    fprintf('\n%s\n', [cdir '/' files])
+  else
+    fprintf('\n%s\n', cdir)
+  end
+  linestr = repmat('-', 1, maxlen*ncols + length(marginstr)*(ncols-1));
+  fprintf([linestr '\n'])
 end
-linestr = repmat('-', 1, maxlen*ncols + length(marginstr)*(ncols-1));
-fprintf([linestr '\n'])
 
-% Get directory list from OS.
+%%% Get directory list from OS.
 ds = dir(files);
 n = length(ds);
 
-% Cull files.
-hitlist = ones(1,n);
-for k = 1:n,
-  d = ds(k);
+%%% Cull files.
+hitlist = true(1,n);
+for krow = 1:n,
+  d = ds(krow);
   namestr = d.name;
   if strcmp(namestr,'.')
-    hitlist(k) = 0;
+    hitlist(krow) = false;
   elseif hidedots && namestr(1) == '.' && ~strcmp(namestr, '..')
-    hitlist(k) = 0;
+    hitlist(krow) = false;
   elseif hideasv && ...
       (strcmp(namestr(max(end-3,1):end),'.asv') || strcmp(namestr(end), '~'))
-    hitlist(k) = 0;
+    hitlist(krow) = false;
   end
 end
 n = sum(hitlist);
-hitks = find(hitlist);
+ds = ds(hitlist);
 
-%%% Make individual strings by type and decide sorting.
-%nameline = char(zeros(n, maxlen));
-nameline = cell(1,n);
-types = zeros(n,1);  % sorting priority
-for k = 1:n,
-  d = ds(hitks(k));
+%%% Check type and do type relevent processing.
+filenames = cell(1,n);
+namestrs = cell(1,n);
+typestrs = cell(1,n);
+sizestrs = cell(1,n);
+typeindex = zeros(n,1);  % sorting priority
+for krow = 1:n,
+  d = ds(krow);
   filestr = d.name;
   
   % Decide on symbol and sorting to use.
   if d.isdir
-    types(k) = -1000;
+    typeindex(krow) = -1000;
     dirsym = '/';
     namestr = filestr;
     opentag = ['<a href="matlab:cd ''' filestr '''; ls">'];
@@ -102,27 +112,27 @@ for k = 1:n,
           firstline = mline;
           fclose(f);
           if strfind(firstline, 'classdef')
-            types(k) = 99;
+            typeindex(krow) = 99;
             dirsym = '@';
           elseif strfind(firstline, 'function')
-            types(k) = 95;     
+            typeindex(krow) = 95;
             dirsym = '*';
           else
-            types(k) = 90;
+            typeindex(krow) = 90;
             dirsym = '$';
           end
         case 'mat',
-          types(k) = 80;
+          typeindex(krow) = 80;
           dirsym = '#';
         case 'fig',
-          types(k) = 70;
+          typeindex(krow) = 70;
           dirsym = '+';
         case 'mdl',
-          types(k) = 60;
+          typeindex(krow) = 60;
           dirsym = '&';
         otherwise,
           dolink = false;
-          types(k) = -int8(typestr(1));
+          typeindex(krow) = -int8(typestr(1));
           dirsym = ['.' typestr];
       end
       namestr = filestr(1:end-typelen-1);
@@ -138,94 +148,101 @@ for k = 1:n,
       namestr = filestr;
     end
   end
-  symlen = length(dirsym);
-  namelen = length(namestr) + symlen;
-  
+  typestrs{krow} = dirsym;
+  namestrs{krow} = namestr;
+  filenames{krow} = filestr;
+
   % Compute file size.
   if d.isdir
     dircount = dircount + 1;
     if countdir
       filesize = dirsize(namestr, depthlimit);
-      sizestr = ['(' makesizestr(filesize) ')'];
+      sizestrs{krow} = ['(' makesizestr(filesize) ')'];
     else
       filesize = 0; %#ok<*UNRCH>
-      sizestr = '-- ';
+      sizestrs{krow} = '-- ';
     end
   else
     filecount = filecount + 1;
     filesize = ceil(d.bytes/1024);
     if strcmp(typestr, 'm')  % m file
       lcount = countmlines(filestr);
-      sizestr = [num2str(lcount) 'L '];
+      sizestrs{krow} = [num2str(lcount) 'L '];
     else
-      sizestr = [makesizestr(filesize) ' '];
+      sizestrs{krow} = [makesizestr(filesize) ' '];
     end
   end
   sizesum = sizesum + filesize;
-  
-  % Build lines and add links.
-  sizelen = length(sizestr);
-  if showi
-    countstr = [num2str(k) ' '];
-    countlen = length(countstr);
+end
+
+%%% Sort by type.
+[~, p] = sort(-typeindex);
+
+%%% Output.
+if printq  % write to terminal
+  nrows = ceil(n/ncols);
+  for krow = 1:nrows,
+    for kcol = 1:ncols,
+      ksort = krow + (kcol-1)*nrows;  % which sorted element we're on
+      if ksort <= n
+        k = p(ksort);
+        namelen = length(namestrs{k});
+        if showi
+          countstr = [num2str(ksort) ' '];
+          countlen = length(countstr);
+        else
+          countstr = '';
+          countlen = 0;
+        end
+        varlen = countlen + length(sizestrs{k}) + length(typestrs{k});
+        if (namelen + varlen) > (maxlen - 1),  % filename too big
+          nameline = [countstr opentag ...
+            namestrs{k}(1:max(maxlen-varlen-contlen-1,1)) ...
+            closetag contstr typestrs{k} ' ' sizestrs{k}];
+        else  % filename fits
+          nameline = [countstr opentag namestrs{k} closetag typestrs{k} ...
+            repmat(fillstr, 1, maxlen-varlen-namelen) sizestrs{k}];
+        end
+        fprintf(nameline)
+        fprintf(marginstr);
+      end
+    end  % for each column
+    fprintf('\n')  % ching-zip!
+  end  % for each row
+else  % write to variable
+  filelist = filenames(p);
+end
+
+%%% Write summary information.
+if printq
+  if summary
+    [mlinecount, mfilecount] = msize('.', 2);
+    sumstr = sprintf('mfiles: %d lines in %d files', ...
+      mlinecount, mfilecount);
+    fprintf([repmat(' ', 1, colwidth - 5 - length(sumstr)) '[' sumstr ']\n'])
+    
+    sumstr = sprintf([makesizestr(sizesum) ' in %d files, %d dirs'], ...
+      filecount, dircount);
+    fprintf([repmat(' ', 1, colwidth - 5 - length(sumstr)) '[' sumstr ']\n'])
   else
-    countstr = '';
-    countlen = 0;
+    fprintf('\n')
   end
-	if (namelen + sizelen) > (maxlen - 1),  % filename too big
-		nameline{k} = [countstr opentag ...
-      namestr(1:max(maxlen-countlen-sizelen-symlen-contlen-1,1)) ...
-      closetag contstr dirsym ' ' sizestr];
-  else  % filename fits
-		nameline{k} = [countstr opentag namestr closetag dirsym ...
-      repmat(fillstr, 1, maxlen-countlen-sizelen-namelen) sizestr];
-  end
-end  % for each individual file string
-
-% Sort.
-[~, p] = sort(-types);
-nameline = nameline(p);
-
-% Output into columns.
-nrows = ceil(n/ncols);
-npad = nrows*ncols - n;
-blankpad = {repmat(' ', 1, size(nameline, 2))};
-nameline = [nameline repmat(blankpad, 1, npad)];  % ensure we have enough
-for k = 1:nrows,
-  for m = 1:(ncols - 1),
-    fprintf(nameline{k + (m-1)*nrows})
-    fprintf(marginstr);
-  end
-  fprintf(nameline{k + (ncols-1)*nrows});
-  fprintf('\n')
-end
-
-% Write summary.
-if summary
-  [mlinecount, mfilecount] = msize('.', 2);
-  sumstr = sprintf('mfiles: %d lines in %d files', ...
-    mlinecount, mfilecount);
-  fprintf([repmat(' ', 1, colwidth - 5 - length(sumstr)) '[' sumstr ']\n'])
   
-  sumstr = sprintf([makesizestr(sizesum) ' in %d files, %d dirs'], ...
-    filecount, dircount);
-  fprintf([repmat(' ', 1, colwidth - 5 - length(sumstr)) '[' sumstr ']\n'])
-else
-  fprintf('\n')
-end
-
-% Output SVN status, if available.
-if ~isempty(dir('.svn'))
-  svnst()
-end
-
-% Output git status, if available.
-if ~isempty(dir('.git'))
-  gitst()
-end
+  % Output SVN status, if available.
+  if ~isempty(dir('.svn')) && showvc
+    svnst()
+  end
   
+  % Output git status, if available.
+  if ~isempty(dir('.git')) && showvc
+    gitst()
+  end
+  
+end % function
 
-% Recursive directory size subroutine.
+
+%%% Recursive directory size subroutine.
+% TODO: See comment in sn.m...
 function sout = dirsize(dirname, depthlimit, level)
 if nargin < 3
   level = 0;
--- nedit.m
+++ nedit.m
@@ -5,27 +5,10 @@ if ischar(k)
   k = str2double(k);
 end
 
-% Get directory list from OS.
-ds = dir(pwd);
-n = length(ds);
+filelist = ls();
 
-% Cull files.
-hitlist = true(1,n);
-for i = 1:n,
-  d = ds(k);
-  namestr = d.name;
-  if strcmp(namestr,'.') || strcmp(namestr,'..')
-    hitlist(i) = false;
-  elseif namestr(1) == '.'
-    hitlist(i) = false;
-  elseif (strcmp(namestr(max(end-3,1):end),'.asv') || strcmp(namestr(end), '~'))
-    hitlist(i) = false;
-  end
-end
-ds = ds(hitlist);
 
-filename = ds(k).name;
+filename = filelist(k);
 open(filename);
 
 end
-
