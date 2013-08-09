385a5afe145efcfa6ccdb2dad6a0c9be0f270930
Jonathan Birge <birge@mit.edu>
2013-08-09 14:18:43 -0400

    fix systemwpath

--- systemwpath.m
+++ systemwpath.m
@@ -1,9 +1,8 @@
 function [status, result] = systemwpath(command)
 %SYSTEMWPATH Execute system shell command with common paths set
-% This command is used to solve a bug (imagine that) that MathWorks
-% introduced on the Mac where the system command doesn't use the user's
-% environment variables. Thus, many commands are not available to the
-% MATLAB version of the system() command.
+% This command is used to solve a bug on the Mac where the system command
+% doesn't use the user's environment variables. Thus, many commands are not
+% available to the MATLAB version of the system() command.
 
 fullcommand = ['PATH=/opt/local/bin:/usr/local/bin:$PATH && ' command];
 [status, result] = system(fullcommand);
