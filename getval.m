231ec901c4d0693d11b57e206761a9eea0c000b7
Jonathan Birge <birge@mit.edu>
2013-08-12 14:16:48 -0400

    update gitval.m documentation

--- getval.m
+++ getval.m
@@ -1,5 +1,9 @@
 function val = getval(tag)
-%GETVAL Retrieve numeric value of edit box object given its tag.
+%GETVAL  Retrieve numeric value associated with GUI object given its tag.
+%  This returns the number in the 'Value' property, if present,
+%  or converts the 'String' property into a number, in the case of an
+%  Edit or Text box. In the latter case, if the string cannot be converted
+%  this function just passes along the NaN given by str2double.
 
 handle = findobj(gcbf, 'Tag', tag);
 style = get(handle, 'Style');
