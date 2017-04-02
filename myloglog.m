c7cdc41b9c8a7c3e5c71341dff0a0b6999157f0d
Jonathan Birge <birge@mit.edu>
2017-04-02 14:42:07 -0400

    fix myloglog.m

--- myloglog.m
+++ myloglog.m
@@ -1,24 +1,16 @@
 function h = myloglog(varargin)
 
-black = [0 0 0];
-white = [1 1 1];
-
-hdls = loglog(varargin{:});
+hdls = myplot(varargin{:});
 
 axh = gca;
-fh = gcf;
-
-set(hdls, 'LineWidth', 2.5, 'Color', [0 .25 0.5], 'MarkerSize', 3)
 
 set(axh, ...
-  'FontName', 'Helvetica', 'Color', white, 'FontSize', 16, 'FontWeight', 'Bold', ...
-  'GridLineStyle', ':', ...
-  'XGrid', 'off', 'XColor', black, 'XMinorTick', 'on', ...
-  'YGrid', 'off', 'YColor', black, 'YMinorTick', 'on')
+  'XGrid', 'on', 'YGrid', 'on', ...
+  'XMinorTick', 'on', 'YMinorTick', 'on', ...
+  'XMinorGrid', 'on', 'YMinorGrid', 'on', ...
+  'YScale', 'log', 'XScale', 'log')
 
-set(fh, 'Color', white)
-
-xdat = get(hdls, 'XData');
+xdat = get(hdls(1), 'XData');
 xlim([min(xdat) max(xdat)])
 
 if nargout > 0
