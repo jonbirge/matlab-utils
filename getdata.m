66d3ff0a5a0a2978700bf33d140e185864fc7400
Jonathan Birge <jonathan.birge@ll.mit.edu>
2020-04-26 20:21:24 -0400

    remove obsolete files

--- getdata.m
+++
@@ -1,6 +0,0 @@
-function data = getdata(tag, prop)
-%GETDATA Retrieve property from GUI given a tag name.
-%  This function is mean to be called from a callback, as it uses gcbf.
-
-handle = findobj(gcbf, 'Tag', tag);
-data = get(handle, prop);
