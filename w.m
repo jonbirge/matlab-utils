1a6f72f50565975af702d86e1d85f2ca7e37b911
Jonathan Birge <jonathan.birge@ll.mit.edu>
2020-04-27 17:22:54 -0400

    implement w.m using tables

--- w.m
+++ w.m
@@ -1,11 +1,29 @@
-function w()
+function vartabout = w()
 %W Print workspace information as a table
+%  The main difference between this and the standard whos command is that
+%  this returns the variables as a MATLAB table object.
 
 vars = evalin('caller', 'whos()');
 n = length(vars);
-fprintf('name\t\t size\t mem\t class\n\n');
+NameC = cell(n, 1);
+SizeC = cell(n, 1);
+MemC = cell(n, 1);
+ClassC = cell(n, 1);
 for k = 1:n
   v = vars(k);
-  fprintf('%s\t %d x %d\t %s\t %s\n', v.name, ...
-    v.size(1), v.size(2), makesizestr(v.bytes/1024), v.class);
+  NameC{k} = v.name;
+  SizeC{k} = [num2str(v.size(1)) ' x ' num2str(v.size(2))];
+  MemC{k} = makesizestr(ceil(v.bytes/1024));
+  ClassC{k} = v.class;
+end
+Size = categorical(SizeC);
+Mem = categorical(MemC);
+Class = categorical(ClassC);
+
+vartab = table(Size, Mem, Class, 'RowNames', NameC);
+
+if nargout == 0
+  disp(vartab)
+else
+  vartabout = vartab;
 end
