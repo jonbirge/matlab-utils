07487c70d43afac33a4fc2127b4e941453c978d8
Jonathan Birge <jonathan.birge@ll.mit.edu>
2020-04-27 21:03:17 -0400

    minor fixes to git wrappers

--- gitlog.m
+++ gitlog.m
@@ -2,12 +2,12 @@ function gitlog
 %GITST Trivial function to get git status
 %  Obtains the status and information about the current directory from the
 %  system git command. Useful only in that it lets you check git status
-%  without having to go to a terminal.
+%  without having to go to a terminal. Only works on Linux or Mac OS X.
 
 [status, result] = systemwpath('git --no-pager log --graph --oneline --decorate --all | head -n 16');
+
 if status == 0
-  fprintf('git log:\n')
-  fprintf(result)
+  disp(result)
 else
   error('gitlog:git', result)
 end
